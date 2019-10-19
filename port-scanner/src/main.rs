37296eaadc964f286e1084e0fe24dc0cf3f63515
Michael Zavarella <mzavarella30@gmail.com>
2019-10-19 14:22:20 -0400

    Update threads to num_threads
    The threads variable name was not descriptive because the variable is actually holding an integer, not a collection of threads. num_threads is more descriptive and will lead to less confusion

--- port-scanner/src/main.rs
+++ port-scanner/src/main.rs
@@ -2,16 +2,18 @@ use structopt::StructOpt;
 use std::net::{IpAddr};
 use std::str::FromStr;
 
+const MAX: u16 = 65535;
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = "port_scanner")]
 struct Opt {
     /// IP of host to scan
     #[structopt(short = "H", long = "host")]
-    host: Option<IpAddr>,
+    host: Option<IpAddr>, // how to handle localhost
 
     /// Number of threads
     #[structopt(short = "t", long = "threads")]
-    threads: Option<u8>,
+    num_threads: Option<u8>,
 
     /// Starting port
     #[structopt(short = "s", long = "start")]
@@ -22,11 +24,14 @@ struct Opt {
     end: u8,
 }
 
+fn scan() {
+}
+
 fn main() {
     let opt = Opt::from_args();
 
     let host = opt.host.unwrap_or_else(|| IpAddr::from_str("127.0.0.1").unwrap());
-    let threads = opt.threads.unwrap_or(4);
+    let num_threads = opt.num_threads.unwrap_or(4);
 
     let start = opt.start;
     let end = opt.end;
