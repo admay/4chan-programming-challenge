c3fca934d9fe10ef3928c79452afd66513830104
Michael Zavarella <mzavarella30@gmail.com>
2019-10-23 10:23:31 -0400

    Add fizz buzz solution

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,3 +1,27 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "fizzbuzz")]
+struct Opt {
+    /// The number to FizzBuzz to
+    #[structopt(short = "n", long = "number")]
+    number: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    let n = opt.number;
+
+    for i in 1..n + 1 {
+        if (i % 5 == 0) && (i % 3 == 0) {
+            println!("FizzBuzz");
+        } else if (i % 5 == 0) && !(i % 3 == 0) {
+            println!("Buzz");
+        } else if !(i % 5 == 0) && (i % 3 == 0) {
+            println!("Fizz");
+        } else {
+            println!("{}", i);
+        }
+    }
 }
